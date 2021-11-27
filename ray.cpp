94fe5cbe55318b1f1c0cad054fd30c988c968f4a
MooOfDoom <lotharnarins@hotmail.com>
2021-11-26 18:59:50 -0800

    Add stat tracking

--- ray.cpp
+++ ray.cpp
@@ -25,6 +25,14 @@
 
 #include "parser.cpp"
 
+typedef struct ray_trace_stats
+{
+	s64 RaysCast;
+	s64 ObjectsChecked;
+	s64 SamplesComputed;
+	s64 Padding[5];
+} ray_trace_stats;
+
 function ray_hit
 RayIntersectScene(v3 RayOrigin, v3 RayDir, scene* Scene)
 {
@@ -157,7 +165,7 @@ RayIntersectScene(v3 RayOrigin, v3 RayDir, scene* Scene)
 }
 
 function void
-RayTrace(scene* Scene, surface* Surface, s32 SamplesPerPixel, s32 MaxBounces, b32 DebugOn)
+RayTrace(scene* Scene, surface* Surface, s32 SamplesPerPixel, s32 MaxBounces, memory_arena* Arena, b32 DebugOn)
 {
 	b32 HitTexture = false; // Debug purposes
 	s32 HitTranslucency = 0; // Debug purposes
@@ -169,158 +177,191 @@ RayTrace(scene* Scene, surface* Surface, s32 SamplesPerPixel, s32 MaxBounces, b3
 	f32 SampleWeight = 1.0f / (SamplesPerPixel*SamplesPerPixel);
 	v3 SurfaceOrigin = Scene->Camera.XAxis*(-0.5f*Scene->Camera.SurfaceWidth + 0.5f*SampleWidth) + Scene->Camera.YAxis*(-0.5f*Scene->Camera.SurfaceHeight + 0.5f*SampleHeight) - Scene->Camera.ZAxis*Scene->Camera.DistToSurface;
 	
-	#pragma omp parallel for
-	for (s32 Y = 0; Y < Surface->Height; ++Y)
+	SetAlignment(Arena, 64); // Make sure to align to cache lines to avoid false sharing
+	ray_trace_stats* AllStats = PushArray(Arena, 0, ray_trace_stats); // Just find the location of the start, reserve the right number later ;)
+	s32 NumThreads;
+	#pragma omp parallel
 	{
-		random_sequence RNG = SeedRandom(4815162342ull*(Y + 1) + 1123581321ull);
-		for (s32 X = 0; X < Surface->Width; ++X)
+		if (omp_get_thread_num() == 0)
 		{
-			color PixelColor = {0};
-			for (s32 J = 0; J < SamplesPerPixel; ++J)
+			NumThreads = omp_get_num_threads();
+			printf("%d Threads...\n", NumThreads);
+		}
+		
+		ray_trace_stats Stats = {};
+		#pragma omp for
+		for (s32 Y = 0; Y < Surface->Height; ++Y)
+		{
+			random_sequence RNG = SeedRandom(4815162342ull*(Y + 1) + 1123581321ull); // Make sure each thread has own random sequence. This keeps it deterministic
+			for (s32 X = 0; X < Surface->Width; ++X)
 			{
-				for (s32 I = 0; I < SamplesPerPixel; ++I)
+				color PixelColor = {0};
+				for (s32 J = 0; J < SamplesPerPixel; ++J)
 				{
-					b32 Debug = (X == 85 && Y == 180 && I == 0 && J == 0);
-					
-					f32 U = (f32)X*PixelWidth + (f32)I*SampleWidth;
-					f32 V = (f32)Y*PixelHeight + (f32)J*SampleHeight;
-					v3 SurfaceX = Scene->Camera.XAxis * U;
-					v3 SurfaceY = Scene->Camera.YAxis * V;
-					
-					v3 RayOrigin = Scene->Camera.Origin;
-					v3 RayDir = NormOrZero(SurfaceOrigin + SurfaceX + SurfaceY);
-					
-					color SampleColor = {1.0f, 1.0f, 1.0f};
-					for (s32 Bounce = 0; Bounce < MaxBounces; ++Bounce)
+					for (s32 I = 0; I < SamplesPerPixel; ++I)
 					{
-						if (DebugOn && Debug)
-						{
-							printf("X(%d) Y(%d) Bounce(%d)\n",
-								X, Y, Bounce);
-							printf("RayOrigin(%.2f,%.2f,%.2f) RayDir(%.2f,%.2f,%.2f)\n",
-								RayOrigin.X, RayOrigin.Y, RayOrigin.Z, RayDir.X, RayDir.Y, RayDir.Z);
-						}
+						++Stats.SamplesComputed;
+						b32 Debug = (X == 85 && Y == 180 && I == 0 && J == 0);
 						
-						ray_hit Hit = RayIntersectScene(RayOrigin, RayDir, Scene);
-						if (Hit.Dist > 0)
+						f32 U = (f32)X*PixelWidth + (f32)I*SampleWidth;
+						f32 V = (f32)Y*PixelHeight + (f32)J*SampleHeight;
+						v3 SurfaceX = Scene->Camera.XAxis * U;
+						v3 SurfaceY = Scene->Camera.YAxis * V;
+						
+						v3 RayOrigin = Scene->Camera.Origin;
+						v3 RayDir = NormOrZero(SurfaceOrigin + SurfaceX + SurfaceY);
+						
+						color SampleColor = {1.0f, 1.0f, 1.0f};
+						for (s32 Bounce = 0; Bounce < MaxBounces; ++Bounce)
 						{
-							RayOrigin = RayOrigin + RayDir*Hit.Dist;
-							f32 RayDDotNormal = Dot(RayDir, Hit.Normal);
+							++Stats.RaysCast;
 							
 							if (DebugOn && Debug)
 							{
-								printf("Debug hit!\n");
-								printf("ObjectType(%d)\n",
-									Hit.Object->Type);
-								printf("HitAt(%.2f,%.2f,%.2f) Normal(%.2f,%.2f,%.2f)\n",
-									RayOrigin.X, RayOrigin.Y, RayOrigin.Z,
-									Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z);
-								printf("RayDDotNormal(%.2f)\n",
-									RayDDotNormal);
+								printf("X(%d) Y(%d) Bounce(%d)\n",
+									X, Y, Bounce);
+								printf("RayOrigin(%.2f,%.2f,%.2f) RayDir(%.2f,%.2f,%.2f)\n",
+									RayOrigin.X, RayOrigin.Y, RayOrigin.Z, RayDir.X, RayDir.Y, RayDir.Z);
 							}
-							f32 Falloff = 1.0f;
 							
-							// Bounce direction
-							f32 Random = RandomUnilateral(&RNG);
-							if (Random < Hit.Object->Translucency)
+							ray_hit Hit = RayIntersectScene(RayOrigin, RayDir, Scene);
+							Stats.ObjectsChecked += Scene->ObjectCount;
+							if (Hit.Dist > 0)
 							{
-								// Pass through the object
-								v3 ParallelComponent = RayDir - Hit.Normal*RayDDotNormal;
-								f32 RefractionCoeff = 1.0f + Hit.Object->Refraction;
-								if (RayDDotNormal < 0)
+								RayOrigin = RayOrigin + RayDir*Hit.Dist;
+								f32 RayDDotNormal = Dot(RayDir, Hit.Normal);
+								
+								if (DebugOn && Debug)
 								{
-									RefractionCoeff = 1.0f / RefractionCoeff;
-								}
-								v3 OldRayDir = RayDir;
-								RayDir = NormOrZero(RayDir - (1.0f - RefractionCoeff)*ParallelComponent);
-								if (DebugOn && HitTranslucency < 10)
-								{
-									++HitTranslucency;
-									Debug = true;
-									printf("Translucency hit! HitAt(%.2f,%.2f,%.2f)\n",
-									RayOrigin.X, RayOrigin.Y, RayOrigin.Z);
-									printf("Bounce(%d) ObjectType(%d) Translucency(%.2f) RefractionCoeff(%.2f)\n",
-										Bounce, Hit.Object->Type, Hit.Object->Translucency, RefractionCoeff);
-									printf("Normal(%.2f,%.2f,%.2f) OldRayDir(%.2f,%.2f,%.2f) NewRayDir(%.2f,%.2f,%.2f)\n",
-										Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z,
-										OldRayDir.X, OldRayDir.Y, OldRayDir.Z,
-										RayDir.X, RayDir.Y, RayDir.Z);
+									printf("Debug hit!\n");
+									printf("ObjectType(%d)\n",
+										Hit.Object->Type);
+									printf("HitAt(%.2f,%.2f,%.2f) Normal(%.2f,%.2f,%.2f)\n",
+										RayOrigin.X, RayOrigin.Y, RayOrigin.Z,
+										Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z);
 									printf("RayDDotNormal(%.2f)\n",
 										RayDDotNormal);
 								}
+								f32 Falloff = 1.0f;
+								
+								// Bounce direction
+								f32 Random = RandomUnilateral(&RNG);
+								if (Random < Hit.Object->Translucency)
+								{
+									// Pass through the object
+									v3 ParallelComponent = RayDir - Hit.Normal*RayDDotNormal;
+									f32 RefractionCoeff = 1.0f + Hit.Object->Refraction;
+									if (RayDDotNormal < 0)
+									{
+										RefractionCoeff = 1.0f / RefractionCoeff;
+									}
+									v3 OldRayDir = RayDir;
+									RayDir = NormOrZero(RayDir - (1.0f - RefractionCoeff)*ParallelComponent);
+									if (DebugOn && HitTranslucency < 10)
+									{
+										++HitTranslucency;
+										Debug = true;
+										printf("Translucency hit! HitAt(%.2f,%.2f,%.2f)\n",
+										RayOrigin.X, RayOrigin.Y, RayOrigin.Z);
+										printf("Bounce(%d) ObjectType(%d) Translucency(%.2f) RefractionCoeff(%.2f)\n",
+											Bounce, Hit.Object->Type, Hit.Object->Translucency, RefractionCoeff);
+										printf("Normal(%.2f,%.2f,%.2f) OldRayDir(%.2f,%.2f,%.2f) NewRayDir(%.2f,%.2f,%.2f)\n",
+											Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z,
+											OldRayDir.X, OldRayDir.Y, OldRayDir.Z,
+											RayDir.X, RayDir.Y, RayDir.Z);
+										printf("RayDDotNormal(%.2f)\n",
+											RayDDotNormal);
+									}
+								}
+								else
+								{
+									// Bounce off
+									// Reflect like a mirror
+									v3 Reflection = RayDir - Hit.Normal*(2.0f*RayDDotNormal);
+									// Reflect randomly
+									v3 RandomBounce = NormOrZero(Hit.Normal + RandomUnitBallV3(&RNG));
+									if (RayDDotNormal > 0)
+									{
+										RandomBounce = -RandomBounce;
+									}
+									RayDir = NormOrDefault(Lerp(RandomBounce, Hit.Object->Glossy, Reflection), Hit.Normal);
+									Falloff = Abs(Dot(RayDir, Hit.Normal));
+								}
+								RayOrigin = RayOrigin + EPSILON*RayDir; // Try to move a little away from the surface to "de-bounce"
+								
+								if (Hit.Object->Texture.Index > 0)
+								{
+									surface* Texture = Scene->Textures + Hit.Object->Texture.Index;
+									v2 SampleUV = Lerp(Hit.Object->UVMap.VertexUV[0], Hit.UV.U, Hit.Object->UVMap.VertexUV[1]) +
+										Lerp(Hit.Object->UVMap.VertexUV[0], Hit.UV.V, Hit.Object->UVMap.VertexUV[2]);
+									s32 SampleX = (s32)(SampleUV.U*(f32)Texture->Width) % Texture->Width;
+									s32 SampleY = (s32)(SampleUV.V*(f32)Texture->Height) % Texture->Height;
+									color TextureColor = Texture->Pixels[SampleY*Texture->Width + SampleX];
+									
+									if (DebugOn && Debug && !HitTexture && Bounce == 0)
+									{
+										HitTexture = true;
+										printf("X(%d) Y(%d)\n",
+											X, Y);
+										printf("ObjectType(%d) Texture(%d)\n",
+											Hit.Object->Type, Hit.Object->Texture.Index);
+										printf("SampleUV(%.2f,%.2f) SampleX(%d) SampleY(%d)\n",
+											SampleUV.U, SampleUV.V, SampleX, SampleY);
+									}
+									
+									// TextureColor = (color){Hit.UV.U < 0.5f ? 0 : 1.0f, Hit.UV.V < 0.5f ? 0 : 1.0f, 0};
+									
+									SampleColor.R *= TextureColor.R*Falloff;
+									SampleColor.G *= TextureColor.G*Falloff;
+									SampleColor.B *= TextureColor.B*Falloff;
+								}
+								else
+								{
+									SampleColor.R *= Hit.Object->Color.R*Falloff;
+									SampleColor.G *= Hit.Object->Color.G*Falloff;
+									SampleColor.B *= Hit.Object->Color.B*Falloff;
+								}
+								
+								if (DebugOn && Debug)
+								{
+									printf("Hit.Object(%d) Hit.Normal(%.2f,%.2f,%.2f) Falloff(%.2f)\n",
+										Hit.Object->Type, Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z, Falloff);
+								}
 							}
 							else
 							{
-								// Bounce off
-								// Reflect like a mirror
-								v3 Reflection = RayDir - Hit.Normal*(2.0f*RayDDotNormal);
-								// Reflect randomly
-								v3 RandomBounce = NormOrZero(Hit.Normal + RandomUnitBallV3(&RNG));
-								if (RayDDotNormal > 0)
-								{
-									RandomBounce = -RandomBounce;
-								}
-								RayDir = NormOrDefault(Lerp(RandomBounce, Hit.Object->Glossy, Reflection), Hit.Normal);
-								Falloff = Abs(Dot(RayDir, Hit.Normal));
-							}
-							RayOrigin = RayOrigin + EPSILON*RayDir; // Try to move a little away from the surface to "de-bounce"
-							
-							if (Hit.Object->Texture.Index > 0)
-							{
-								surface* Texture = Scene->Textures + Hit.Object->Texture.Index;
-								v2 SampleUV = Lerp(Hit.Object->UVMap.VertexUV[0], Hit.UV.U, Hit.Object->UVMap.VertexUV[1]) +
-									Lerp(Hit.Object->UVMap.VertexUV[0], Hit.UV.V, Hit.Object->UVMap.VertexUV[2]);
-								s32 SampleX = (s32)(SampleUV.U*(f32)Texture->Width) % Texture->Width;
-								s32 SampleY = (s32)(SampleUV.V*(f32)Texture->Height) % Texture->Height;
-								color TextureColor = Texture->Pixels[SampleY*Texture->Width + SampleX];
-								
-								if (DebugOn && Debug && !HitTexture && Bounce == 0)
-								{
-									HitTexture = true;
-									printf("X(%d) Y(%d)\n",
-										X, Y);
-									printf("ObjectType(%d) Texture(%d)\n",
-										Hit.Object->Type, Hit.Object->Texture.Index);
-									printf("SampleUV(%.2f,%.2f) SampleX(%d) SampleY(%d)\n",
-										SampleUV.U, SampleUV.V, SampleX, SampleY);
-								}
-								
-								// TextureColor = (color){Hit.UV.U < 0.5f ? 0 : 1.0f, Hit.UV.V < 0.5f ? 0 : 1.0f, 0};
-								
-								SampleColor.R *= TextureColor.R*Falloff;
-								SampleColor.G *= TextureColor.G*Falloff;
-								SampleColor.B *= TextureColor.B*Falloff;
-							}
-							else
-							{
-								SampleColor.R *= Hit.Object->Color.R*Falloff;
-								SampleColor.G *= Hit.Object->Color.G*Falloff;
-								SampleColor.B *= Hit.Object->Color.B*Falloff;
-							}
-							
-							if (DebugOn && Debug)
-							{
-								printf("Hit.Object(%d) Hit.Normal(%.2f,%.2f,%.2f) Falloff(%.2f)\n",
-									Hit.Object->Type, Hit.Normal.X, Hit.Normal.Y, Hit.Normal.Z, Falloff);
+								SampleColor.R *= Scene->SkyColor.R;
+								SampleColor.G *= Scene->SkyColor.G;
+								SampleColor.B *= Scene->SkyColor.B;
+								break;
 							}
 						}
-						else
-						{
-							SampleColor.R *= Scene->SkyColor.R;
-							SampleColor.G *= Scene->SkyColor.G;
-							SampleColor.B *= Scene->SkyColor.B;
-							break;
-						}
+						
+						PixelColor.R += SampleColor.R;
+						PixelColor.G += SampleColor.G;
+						PixelColor.B += SampleColor.B;
 					}
-					
-					PixelColor.R += SampleColor.R;
-					PixelColor.G += SampleColor.G;
-					PixelColor.B += SampleColor.B;
 				}
+				Surface->Pixels[Y*Surface->Width + X] = PixelColor*SampleWeight;
 			}
-			Surface->Pixels[Y*Surface->Width + X] = PixelColor*SampleWeight;
+			s32 ThreadNum = omp_get_thread_num();
+			AllStats[ThreadNum] = Stats;
 		}
 	}
+	PushArray(Arena, NumThreads, ray_trace_stats); // Actually reserve the space :)
+	
+	ray_trace_stats OverallStats = {};
+	for (s32 Index = 0; Index < NumThreads; ++Index)
+	{
+		printf("Thread %d: %ld rays cast, %ld objects checked, %ld samples computed\n",
+			Index, AllStats[Index].RaysCast, AllStats[Index].ObjectsChecked, AllStats[Index].SamplesComputed);
+		OverallStats.RaysCast += AllStats[Index].RaysCast;
+		OverallStats.ObjectsChecked += AllStats[Index].ObjectsChecked;
+		OverallStats.SamplesComputed += AllStats[Index].SamplesComputed;
+	}
+	printf("--------\n");
+	printf("Overall: %ld rays cast, %ld objects checked, %ld samples computed\n",
+		OverallStats.RaysCast, OverallStats.ObjectsChecked, OverallStats.SamplesComputed);
 }
 
 function surface
@@ -534,7 +575,7 @@ main(int ArgCount, char** Args)
 			
 			std::chrono::time_point<std::chrono::high_resolution_clock> StartTime = std::chrono::high_resolution_clock::now();
 			
-			RayTrace(&Scene, &Surface, Options.SamplesPerPixel, Options.MaxBounces, Options.Debug);
+			RayTrace(&Scene, &Surface, Options.SamplesPerPixel, Options.MaxBounces, &Arena, Options.Debug);
 			
 			std::chrono::time_point<std::chrono::high_resolution_clock> EndTime = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> ElapsedTime = EndTime - StartTime;
